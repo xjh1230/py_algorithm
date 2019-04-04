4730900511b302b755948bf0d745c0c7f0febe67
邢建辉 <xjh_0125@sina.com>
2019-04-04 13:06:43 +0800

    1

--- test/edit_distance.py
+++ test/edit_distance.py
@@ -28,10 +28,12 @@ class Solution:
         rorse -> rose (删除 'r')
         rose -> ros (删除 'e')
         dp(dynamic programming)实现
+        https://mp.weixin.qq.com/s?__biz=MjM5MTYwMjI3Mw==&mid=2652097703&idx=1&sn=ba293c267fda9629bb41eaf6fb4bfc89&chksm=bd546fa98a23e6bf5baaf888596f7fadef3b883e022921a5fe1563c5f1da2425ef72b844497b&scene=0&xtrack=1&key=60f932dd5bc68039014180b741c41c59b6e5aa51b976d139e76f0a37931f59a70d43e89b8587b5c09f53689619218cecff6a9c0fd943c75c9cac899ec93cdbf61dcf8212503be7f1463fc1c84dc4be6a&ascene=1&uin=MTg4Njc3MTc2MA%3D%3D&devicetype=Windows+10&version=62060739&lang=zh_CN&pass_ticket=k6vNurw8LWlL%2Bsn4Cpk%2F7ZPfY1vJr%2BCgUQ%2F9gP7jLlkGu9sALwv9jDu4tj%2BJeU3Q
+        没看懂
         '''
         pass
 
-    def minDistance(self, word1: str, word2: str) -> int:
+    def minDistance1(self, word1: str, word2: str) -> int:
         # 初始化matrix第一行为0到n，第一列为0到m。
         # Matrix[0][j]表示第1行第j-1列的值，这个值表示将串s[1…0]转换为t[1..j]所需要执行的操作的次数，
         # 很显然将一个空串转换为一个长度为j的串，只需要j次的add操作，所以matrix[0][j]的值应该是j，
@@ -41,7 +43,7 @@ class Solution:
         for i in range(m + 1):
             matrix[i][0] = i
         for i in range(n + 1):
-            matrix[0][i] = i
+            matrix[0][i] = i#0
         for i in range(1, m + 1):
             tmp_a = word1[i - 1]
             for j in range(1, n + 1):
@@ -51,7 +53,7 @@ class Solution:
                 else:
                     tmp_count = 1
                 matrix[i][j] = min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + tmp_count)
-        print(m, n)
+        # print(m, n)
         for i in matrix:
             print(i)
         return matrix[m][n]
@@ -60,7 +62,7 @@ class Solution:
         ret = [{} for _ in range(len(word1) + 1)]
 
         def getDistance(i, j):
-            print(i, j)
+            # print(i, j)
             # if i == 0: return j
             # if j == 0: return i
             if i * j == 0: return i + j
@@ -76,10 +78,7 @@ class Solution:
         return getDistance(len(word1), len(word2))
 
 
-
-
-
 if __name__ == '__main__':
     s = Solution()
-    print(s.minDistance('intention', 'execution'))
-
+    # print(s.minDistance1('intention', 'execution'))
+    print(s.minDistance1('annual', 'annealing'))
