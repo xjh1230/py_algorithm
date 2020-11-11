cd32d5b9e5206b793caf37c6bb90a9f870bb5736
xjh1230 <xjh_0125@sina.com>
2020-11-11 17:31:36 +0800

    vscode leetcode 配置

---
+++ test/78.子集.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=78 lang=python3
+#
+# [78] 子集
+#
+
+# @lc code=start
+class Solution:
+    def subsets(self, nums):
+        print(123)
+
+if __name__ == '__main__':
+    print(123)
+    
+# @lc code=end
+
